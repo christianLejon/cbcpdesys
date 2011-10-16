214285d4caca671399380c21d29364c775404543
Mikael Mortensen <mikael.mortensen@gmail.com>
2011-10-16 12:45:32 +0200

    more testing

--- cbc/pdesys/__init__.py
+++ cbc/pdesys/__init__.py
@@ -2,7 +2,6 @@ __author__ = "Mikael Mortensen <Mikael.Mortensen@ffi.no>"
 __date__ = "2010-09-08"
 __copyright__ = "Copyright (C) 2010 " + __author__
 __license__  = "GNU GPL version 3 or any later version"
-
 from PDESubSystems import *
 from PDESystem import *
 from Problem import *
